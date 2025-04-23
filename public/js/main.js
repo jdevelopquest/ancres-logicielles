93bf3d455a80f2a4ddbe489fbef8c851c8a10b75
jacques <jacques@jdevelop.com>
2025-04-23 17:26:59 +0200

    Premier résultat "mobil first".

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('button-hamburger').addEventListener('click', function () {
-        const menu = document.getElementById('menu-hamburger');
+    document.querySelector('.button-hamburger')?.addEventListener('click', function () {
+        const menu = document.querySelector('.menu-hamburger');
         if (menu.style.display === 'block') {
             menu.style.display = 'none'; // Fermer le menu
         } else {
@@ -8,9 +8,18 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    document.getElementById('button-switch-theme').addEventListener('click', function () {
-        const body = document.getElementById('body');
-        body.classList.toggle('dark-theme');
-        body.classList.toggle('light-theme');
+    document.querySelector('.button-admin')?.addEventListener('click', function () {
+        const menu = document.querySelector('.menu-admin');
+        if (menu.style.display === 'block') {
+            menu.style.display = 'none'; // Fermer le menu
+        } else {
+            menu.style.display = 'block'; // Ouvrir le menu
+        }
+    });
+
+    document.querySelector('.button-switch-theme')?.addEventListener('click', function () {
+        const body = document.querySelector('body');
+        body.classList.toggle('theme-light');
+        body.classList.toggle('theme-dark');
     });
 })
--- src/Views/layouts/main.php
+++ src/Views/layouts/main.php
@@ -1,57 +1,74 @@
-<?php
-$al_hamburger_icon = "<svg viewBox=\"0 0 64 64\" xmlns=\"http://www.w3.org/2000/svg\"> <path d=\"M 0,0 V 16 H 64 V 0 Z M 0,24 V 40 H 64 V 24 Z M 0,48 V 64 H 64 V 48 Z\"/></svg>";
-$al_switch_theme_icon = "<svg viewBox=\"0 0 64 64\" xmlns=\"http://www.w3.org/2000/svg\"> <path d=\"m 0,0 v 64 h 64 v -64 z m 32,4 h 28 v 56 h -28 z\"/></svg>";
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="icon" type="image/svg" href="img/al-favicon.svg"/>
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/svg" href="/public/img/al-favicon.svg">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap">
+    <link rel="stylesheet" href="/public/css/style.css">
+
     <title>Ancres Logicielles</title>
 </head>
-<body class="body light-theme" id="body">
+<body class="theme-light">
 <div class="container">
-    <header class="header">
-        <div class="panel panel-secondary">
-            <span class="logo logo-part-0">A</span>
-            <span class="logo logo-part-1">ncres</span>
-            <button class="button button-hamburger" id="button-hamburger">
-                <?= $al_hamburger_icon ?>
-            </button>
+    <header>
+        <div class="panel">
+            <h1 class="text">Ancres Logicielles</h1>
+            <button class="tool icon button-hamburger"></button>
         </div>
-        <div class="panel panel-primary">
-            <span class="logo logo-part-2">Logicielles</span>
-        </div>
-        <div class="menu menu-hamburger" id="menu-hamburger">
-            <nav class="menu-hamburger__navbar" id="navbar">
-                <ul>
-                    <li class="panel panel-secondary"><a class="button" href="#home">Accueil</a></li>
-                    <li class="panel panel-secondary"><a class="button" href="#signin">Inscription</a></li>
-                    <li class="panel panel-secondary"><a class="button" href="#login">Connexion</a></li>
-                </ul>
-            </nav>
-            <div class="menu-hamburger__action">
-                <ul>
-                    <li class="panel panel-secondary">
-                        <span>Thème</span>
-                        <button class="button button-switch-theme" id="button-switch-theme">
-                            <?= $al_switch_theme_icon ?>
-                        </button>
-                    </li>
-                </ul>
-            </div>
+        <div class="panel">
+            <button class="tool icon button-previous-page"></button>
+            <h2 class="text">Accueil</h2>
+            <button class="tool icon button-admin"></button>
         </div>
     </header>
-    <main class="content">
-        {{content}}
+    <main>
+        <aside class="sidebar menu-hamburger">
+            <nav>
+                <a class="panel tool">
+                    <span class="text">Accueil</span>
+                    <span class="icon"></span>
+                </a>
+                <a class="panel tool">
+                    <span class="text">Inscription</span>
+                    <span class="icon"></span>
+                </a>
+                <a class="panel tool">
+                    <span class="text">Connexion</span>
+                    <span class="icon"></span>
+                </a>
+            </nav>
+            <div class="panel toolbox">
+                <button class="tool icon button-switch-theme" title="Changer le thème"></button>
+            </div>
+        </aside>
+        <section>
+            <nav>
+                <a class="panel tool">
+                    <span class="text">Blender</span>
+                    <span class="icon"></span>
+                </a>
+            </nav>
+        </section>
+        <aside class="sidebar menu-admin">
+            <nav>
+                <a class="panel tool">
+                    <span class="text">Ajouter une fiche</span>
+                    <span class="icon"></span>
+                </a>
+            </nav>
+        </aside>
     </main>
-    <script src="js/main.js" type="module"></script>
-    <footer class="footer"></footer>
+    <footer>
+        <nav>
+            <a class="panel tool">
+                <span class="text">À propos</span>
+                <span class="icon"></span>
+            </a>
+        </nav>
+    </footer>
 </div>
+<script src="/public/js/main.js" type="module"></script>
 </body>
 </html>
