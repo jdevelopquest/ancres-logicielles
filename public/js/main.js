7d6a46f2f8499905ba74cc73092557d0e975264c
jacques <jacques@jdevelop.com>
2025-04-26 14:18:35 +0200

    Base du sass.

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('.button-hamburger')?.addEventListener('click', function () {
-        const menu = document.querySelector('.menu-hamburger');
-        if (menu.style.display === 'block') {
-            menu.style.display = 'none'; // Fermer le menu
-        } else {
-            menu.style.display = 'block'; // Ouvrir le menu
-        }
+function initMenu(menuTag) {
+    const buttonSelector = '.button-menu-' + menuTag;
+    const menuSelector = '.menu-' + menuTag;
+    document.querySelector(buttonSelector)?.addEventListener('click', function () {
+        const menu = document.querySelector(menuSelector);
+        menu?.classList.toggle('hide');
+        const close = menu?.querySelector('.button-close');
+        close?.addEventListener('click', function () {
+            menu?.classList.add('hide');
+        });
     });
+}
 
-    document.querySelector('.button-admin')?.addEventListener('click', function () {
-        const menu = document.querySelector('.menu-admin');
-        if (menu.style.display === 'block') {
-            menu.style.display = 'none'; // Fermer le menu
-        } else {
-            menu.style.display = 'block'; // Ouvrir le menu
-        }
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    initMenu('hamburger');
+    initMenu('tiny');
 
     document.querySelector('.button-switch-theme')?.addEventListener('click', function () {
         const body = document.querySelector('body');
-        body.classList.toggle('theme-light');
-        body.classList.toggle('theme-dark');
+        body?.classList.toggle('theme-light');
+        body?.classList.toggle('theme-dark');
     });
 })
