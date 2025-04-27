186d5f198cd2478bc70723ba991882507eb2348a
jacques <jacques@jdevelop.com>
2025-04-27 23:36:25 +0200

    Ajout d'ancres haut et bas de page dans le menu-tiny.

--- src/Views/layouts/menu-tiny.php
+++ src/Views/layouts/menu-tiny.php
@@ -1,9 +1,13 @@
 <aside class="menu menu-tiny hide">
+    <div class="menu__submenu menu__submenu-v">
+        <a class="menu__item menu__item-clickable prominent" title="Aller en haut de la page" href="#page-top"><span class="prominent__icon icon-go-top"></span></a>
+    </div>
+
     <?php if(isset($menu)): ?>
         <?php foreach($menu as $submenu): ?>
             <?= "<nav class=\"menu__submenu menu__submenu-v\">" ?>
             <?php foreach($submenu as $item): ?>
-                <?= "<a class=\"menu__item menu__item-clickable prominent\" href=\"{$item['href']}\" title=\"{$item['href']}\"><span class=\"prominent__icon icon-{$item['icon']}\"></span></a>" ?>
+                <?= "<a class=\"menu__item menu__item-clickable prominent\" title=\"{$item['href']}\" href=\"{$item['href']}\"><span class=\"prominent__icon icon-{$item['icon']}\"></span></a>" ?>
             <?php endforeach; ?>
             <?= "</nav>" ?>
         <?php endforeach; ?>
@@ -13,5 +17,8 @@
         <button class="menu__item menu__item-clickable prominent button-switch-theme" title="Changer de thème"><span class="prominent__icon icon-theme"></span></button>
         <button class="menu__item menu__item-clickable prominent button-menu-tiny" title="Fermer ce menu"><span class="prominent__icon icon-menu"></span></button>
     </div>
-</aside>
 
+    <div class="menu__submenu menu__submenu-v">
+        <a class="menu__item menu__item-clickable prominent" title="Aller au bas de la page" href="#page-bot"><span class="prominent__icon icon-go-bot"></span></a>
+    </div>
+</aside>
