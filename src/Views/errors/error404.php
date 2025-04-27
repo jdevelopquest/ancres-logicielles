61004cb8f537e9e7415175ae65ec3ed0495bece1
jacques <jacques@jdevelop.com>
2025-04-27 16:19:44 +0200

    La page error404.php fonctionne.

---
+++ src/Views/errors/error404.php
@@ -0,0 +1,14 @@
+<section class="content">
+    <div class="content__subcontent prominent">
+        <h2 class="prominent__text">Erreur</h2>
+    </div>
+    <article class="content__subcontent">
+        <div class="prominent">
+            <h3 class="prominent__text">404</h3>
+            <span class="prominent__icon"></span>
+        </div>
+        <div class="regular">
+            <p class="regular__text">Le serveur n'a pas trouvé la ressource demandée.</p>
+        </div>
+    </article>
+</section>
