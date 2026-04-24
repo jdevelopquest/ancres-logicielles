43e0163af04a85aadd9e6e3e95a192dfc9b6c93a
jdevelopquest <212350105+jdevelopquest@users.noreply.github.com>
2026-04-24 19:51:07 +0200

    Add timestamps to log messages for better traceability
    Updated the `logMessage` and `logData` methods to include timestamps in the log entries. This improves the ability to track when specific log messages were produced.

--- src/Application/Utils/LogPrinter.php
+++ src/Application/Utils/LogPrinter.php
@@ -7,14 +7,14 @@ trait LogPrinter
     protected function logMessage(string $message): void
     {
         $file = LOG . "messages.log";
-        $message = sprintf("%s\n", $message);
+        $message = sprintf("[%s] %s\n", date('Y-m-d H:i:s'), $message);
         error_log($message, 3, $file);
     }
 
     protected function logData(mixed $data): void
     {
         $file = LOG . "messages.log";
-        $message = sprintf("%s\n", var_export($data, true));
+        $message = sprintf("[%s] %s\n", date('Y-m-d H:i:s'), var_export($data, true));
         error_log($message, 3, $file);
     }
 }
