c30b695fbffc7f3b32f98b68bcaf2af3678a647a
jdevelopquest <212350105+jdevelopquest@users.noreply.github.com>
2026-04-24 19:51:07 +0200

    Refactor logging in LogPrinter and remove IDE configs.
    Refactored the LogPrinter trait to centralize logging logic in a private method, improving code reuse and maintainability. Removed `.idea` directory configurations from version control to declutter the repository and exclude IDE-specific files.

--- src/Application/Utils/LogPrinter.php
+++ src/Application/Utils/LogPrinter.php
@@ -4,17 +4,41 @@ namespace App\Application\Utils;
 
 trait LogPrinter
 {
+    private const LOG_FILE = "messages.log";
+    private const DATE_FORMAT = 'Y-m-d H:i:s';
+
+    /**
+     * Logs a message to the designated logging mechanism.
+     *
+     * @param string $message The message to be logged.
+     * @return void
+     */
     protected function logMessage(string $message): void
     {
-        $file = LOG . "messages.log";
-        $message = sprintf("[%s] %s\n", date('Y-m-d H:i:s'), $message);
-        error_log($message, 3, $file);
+        $this->writeToLog($message);
     }
 
+    /**
+     * Logs the provided data to the designated logging mechanism.
+     *
+     * @param mixed $data The data to be logged.
+     * @return void
+     */
     protected function logData(mixed $data): void
     {
-        $file = LOG . "messages.log";
-        $message = sprintf("[%s] %s\n", date('Y-m-d H:i:s'), var_export($data, true));
-        error_log($message, 3, $file);
+        $this->writeToLog(var_export($data, true));
+    }
+
+    /**
+     * Writes a formatted message to the log file.
+     *
+     * @param string $content The content to be written to the log.
+     * @return void
+     */
+    private function writeToLog(string $content): void
+    {
+        $logFile = LOG . self::LOG_FILE;
+        $formattedMessage = sprintf("[%s] %s\n", date(self::DATE_FORMAT), $content);
+        error_log($formattedMessage, 3, $logFile);
     }
 }
