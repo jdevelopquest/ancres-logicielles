86e722f4825f7a719b93258f465003c227257f77
jdevelopquest <212350105+jdevelopquest@users.noreply.github.com>
2026-05-07 16:50:34 +0200

    Replace elvis operator by Null Coalescingin config/databases

--- app/config/databases.php
+++ app/config/databases.php
@@ -3,10 +3,10 @@ use App\Core\DatabaseMariaDB;
 define(
     constant_name: "DATABASE_MARIADB",
     value: new DatabaseMariaDB(
-        driver: getenv("DATABASE_DRIVER") ?: "mysql",
-        host: getenv("DATABASE_HOST") ?: "database",
-        name: getenv("DATABASE_DATABASE") ?: "ancres-logicielles",
-        password: getenv("DATABASE_PASSWORD") ?: "al-password",
-        username: getenv("DATABASE_USER") ?: "al",
+        driver: getenv("DATABASE_DRIVER") ?? "mysql",
+        host: getenv("DATABASE_HOST") ?? "database",
+        name: getenv("DATABASE_DATABASE") ?? "ancres-logicielles",
+        password: getenv("DATABASE_PASSWORD") ?? "al-password",
+        username: getenv("DATABASE_USER") ?? "al",
     ),
 );
