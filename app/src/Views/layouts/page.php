9ad894fd71c42ec26da26199e400699e85ef5efa
jdevelopquest <212350105+jdevelopquest@users.noreply.github.com>
2026-07-21 18:17:50 +0200

    Create a menu__item-rightcorner css class

--- app/src/Views/layouts/page.php
+++ app/src/Views/layouts/page.php
@@ -28,7 +28,7 @@ $theme = $theme ?? "theme-light";
                     <h1 class="menu__item prominent__text">Ancres Logicielles</h1>
                 </a>
             </nav>
-            <button class="menu__item menu__item-clickable prominent button-menu-hamburger"
+            <button class="menu__item menu__item-rightcorner menu__item-clickable prominent button-menu-hamburger"
                     title="Ouvrir ou fermer le menu hamburger"><span class="prominent__icon icon-menu-hamburger"></span>
             </button>
         </div>
